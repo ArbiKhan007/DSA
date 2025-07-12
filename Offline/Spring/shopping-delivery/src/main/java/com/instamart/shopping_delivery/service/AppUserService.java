4ee66964caa766d7cbac893fe691e59da83d5fe2
Somendrat22 <somendrat22@gmail.com>
2025-07-12 17:43:46 +0530

    Warehouse registration and customer registration done

--- Offline/Spring/shopping-delivery/src/main/java/com/instamart/shopping_delivery/service/AppUserService.java
+++ Offline/Spring/shopping-delivery/src/main/java/com/instamart/shopping_delivery/service/AppUserService.java
@@ -1,9 +1,13 @@
 package com.instamart.shopping_delivery.service;
 
+import com.instamart.shopping_delivery.exceptions.InvalidOperationException;
+import com.instamart.shopping_delivery.exceptions.UserNotExistException;
 import com.instamart.shopping_delivery.models.AppUser;
 import com.instamart.shopping_delivery.repositories.AppUserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.UUID;
 
 @Service
 public class AppUserService {
@@ -15,9 +19,21 @@ public class AppUserService {
        this.appUserRepository = appUserRepository;
    }
 
-    public void saveUser(AppUser user){
-        appUserRepository.save(user);
-        appUserRepository.findAll();
-        appUserRepository.delete(user);
-    }
+   public AppUser registerCustomer(AppUser customer){
+       return appUserRepository.save(customer);
+   }
+
+   public AppUser wareHouseAdminInvite(UUID userId, AppUser wareHouseAdmin){
+       // 1. First verify user who is inviting is of type appadmin or not.
+       AppUser admin = appUserRepository.findById(userId).orElse(null);
+       if(admin == null){
+           // Throw exception that user does not exist
+           throw new UserNotExistException("user does not exist");
+       }
+       if(!admin.getUserType().equals("APP_ADMIN")){
+           throw new InvalidOperationException("User is not allowed to invite warehosue admim");
+       }
+       wareHouseAdmin.setStatus("INACTIVE");
+       return appUserRepository.save(wareHouseAdmin);
+   }
 }
