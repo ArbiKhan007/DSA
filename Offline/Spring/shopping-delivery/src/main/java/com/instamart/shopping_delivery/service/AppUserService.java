97bce2ec67f30940a509c17a881dab61e3df967f
Somendrat22 <somendrat22@gmail.com>
2025-07-12 18:20:48 +0530

    Mail invitation flow developed

--- Offline/Spring/shopping-delivery/src/main/java/com/instamart/shopping_delivery/service/AppUserService.java
+++ Offline/Spring/shopping-delivery/src/main/java/com/instamart/shopping_delivery/service/AppUserService.java
@@ -4,9 +4,14 @@ import com.instamart.shopping_delivery.exceptions.InvalidOperationException;
 import com.instamart.shopping_delivery.exceptions.UserNotExistException;
 import com.instamart.shopping_delivery.models.AppUser;
 import com.instamart.shopping_delivery.repositories.AppUserRepository;
+import jakarta.mail.internet.MimeMessage;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
+import java.util.Properties;
 import java.util.UUID;
 
 @Service
@@ -28,12 +33,37 @@ public class AppUserService {
        AppUser admin = appUserRepository.findById(userId).orElse(null);
        if(admin == null){
            // Throw exception that user does not exist
-           throw new UserNotExistException("user does not exist");
+           throw new UserNotExistException(String.format("User with id %s does not exist", userId.toString()));
        }
        if(!admin.getUserType().equals("APP_ADMIN")){
            throw new InvalidOperationException("User is not allowed to invite warehosue admim");
        }
        wareHouseAdmin.setStatus("INACTIVE");
+       this.sendInvitationMail(wareHouseAdmin);
        return appUserRepository.save(wareHouseAdmin);
+   }
+
+   public void sendInvitationMail(AppUser wareHouseAdmin){
+       JavaMailSenderImpl javaMailSender = new JavaMailSenderImpl();
+       javaMailSender.setHost("smtp.gmail.com");
+       javaMailSender.setPort(587);
+       javaMailSender.setUsername("accioshoppingwebsite@gmail.com");
+       javaMailSender.setPassword("relcfdwhahhcvokv");
+       Properties props = javaMailSender.getJavaMailProperties();
+       props.put("mail.smtp.auth", "true");
+       props.put("mail.smtp.starttls.enable", "true");
+       // Mime message ke andar hum mail ka content set karte hai
+       MimeMessage mimeMessage = javaMailSender.createMimeMessage();
+       // Mime message ke andar bhi hum directly content set nahi kar sakte hume banana hota hai mimeMessage helper
+       MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage);
+       try{
+           mimeMessageHelper.setTo(wareHouseAdmin.getEmail());
+           mimeMessageHelper.setSubject("Invitation to our swiggy platform as warehouse amdin");
+           mimeMessageHelper.setText("Hey " + wareHouseAdmin.getName());
+       }catch (Exception e){
+
+       }
+
+       javaMailSender.send(mimeMessage);
    }
 }
