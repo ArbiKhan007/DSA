701bc0f40def74cdc5fdce7a65a18191574dd3f8
Somendra Tiwari <49708374+somendrat22@users.noreply.github.com>
2025-08-24 17:52:25 +0530

    Create VariablePractice1.java

---
+++ DSA-HYD-2/Module1/Basics/VariablePractice1.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String args[]) {
+        int var = 10;
+        // String str = "My num is " + var;
+        // System.out.println(str);
+        System.out.print("My num is ");
+        System.out.print(var);
+    }
+}
