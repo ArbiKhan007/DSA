450ac7fdcfe364f6e5b1ea242c9bb3e291ed8db9
Somendra Tiwari <49708374+somendrat22@users.noreply.github.com>
2025-08-24 17:52:59 +0530

    Create VariablePractice4.java

---
+++ DSA-HYD-2/Module1/Basics/VariablePractice4.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        int a = 1042;
+        int b = 7;
+        int quo = a/b;
+        int rem = a%b;
+        System.out.println(quo + " " + rem); 
+    }
+}
