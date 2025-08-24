228504adfd195e6cb3ae0027647702dce4d5f564
Somendra Tiwari <49708374+somendrat22@users.noreply.github.com>
2025-08-24 17:54:21 +0530

    Create AgentBinod.java

---
+++ DSA-HYD-2/Module1/Basics/AgentBinod.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+         Scanner scn = new Scanner(System.in);
+         String agentName = scn.nextLine();
+         System.out.println("Hi my name is Agent " + agentName);
+    }
+}
