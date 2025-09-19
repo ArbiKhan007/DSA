737812314e4b9fcb8ef13ae8eab3645da9f218bc
Somendra Tiwari <49708374+somendrat22@users.noreply.github.com>
2025-09-19 14:26:59 +0530

    Update set_airflow_variable.py

--- JavaDaily/set_airflow_variable.py
+++ JavaDaily/set_airflow_variable.py
@@ -1,6 +1,5 @@
 import logging
 from airflow_client.client.api.variable_api import VariableApi
-from airflow_client.client.model.variable import Variable
 from airflow_client.client.exceptions import ApiException
 from airflow_connection import AirflowConnectionClass
 
