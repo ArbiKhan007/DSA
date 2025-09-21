0b42d878dc941eb5025fff190ecdd6598301eb9d
Somendra Tiwari <49708374+somendrat22@users.noreply.github.com>
2025-09-21 17:53:20 +0530

    Create SecondLargest.java

---
+++ DSA-HYD-2/Module1/Arrays/SecondLargest.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.*;
+
+
+public class Main {
+    public static void SecondLargest(int[] arr, int n) {
+        int max = Integer.MIN_VALUE;
+        int smax = Integer.MIN_VALUE;
+        for(int i = 0; i < arr.length; i++){
+            if(arr[i] > max){
+                smax = max;
+                max = arr[i];
+            }else if(arr[i] > smax){
+                smax = arr[i];
+            }
+        }
+        System.out.println(smax);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n= sc.nextInt();
+        int[] arr= new int[n];
+
+        for(int i=0;i<n;i++)
+            arr[i]=sc.nextInt();
+    
+        SecondLargest(arr,n);
+    }
+}
