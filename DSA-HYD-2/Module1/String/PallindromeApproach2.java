9fd723cab9bbfdce5b50f84a7720d86fbf222bb9
Somendra Tiwari <49708374+somendrat22@users.noreply.github.com>
2025-10-26 16:16:24 +0530

    Create PallindromeApproach2.java

---
+++ DSA-HYD-2/Module1/String/PallindromeApproach2.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	static int isPalindrome(String str)
+    {
+        StringBuilder res = new StringBuilder("");
+        for(int i = 0; i < str.length(); i++){
+            char ch = str.charAt(i);
+            if(ch >= 'a' && ch <= 'z'){
+                res.append(ch);
+            }else if(ch >= 'A' && ch <= 'Z'){
+                res.append((char)(ch - 'A' + 'a'));
+            }
+        }
+        int i = 0;
+        int j = res.length() - 1;
+        while(i < j){
+            char chi = res.charAt(i);
+            char chj = res.charAt(j);
+            if(chi != chj){
+                return 0;
+            }
+            i++;
+            j--;
+        }
+        return 1;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str=sc.nextLine();
+        int result = isPalindrome(str);
+        System.out.println(result);        
+    }
+}
